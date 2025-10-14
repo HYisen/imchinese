a61a4123f941418ebf9a3dda7f49949769dc5abe
Alex <alexhyisen@gmail.com>
2025-10-14 18:51:09 +0800

    switch to external input in main

--- main.go
+++ main.go
@@ -3,19 +3,16 @@ package main
 import (
 	"fmt"
 	"imchinese/finder"
+	"log"
+	"os"
 )
 
 func main() {
-	text := `## 缘起
-
-> BTW I use Arch
-
-所以你可以猜到我会选择 latest。作为 edge-runner 除了冒着相信上游的风险外，也是背上了时刻更新的负担。那看都看了，不妨留下记录，未来就可以直接过而无需在 déjà vu 中纠结是否曾经考虑过这个问题。
-
-另外我的一贯作风是把 latest 作为常态基准，在特定情况下难以更新的话，就是一个 TODO 甩上去表示以后会如何使用新特性改善。了解更新历史为兼容旧版本提供可能，尽管我有在尽力避免不能滚动更新的情况。
-
-`
-	prettyPrint(finder.Find(text))
+	data, err := os.ReadFile("text.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+	prettyPrint(finder.Find(string(data)))
 }
 
 func prettyPrint(words []string) {
